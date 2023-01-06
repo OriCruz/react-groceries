66f91288494352b2ac3873c778cad89dc5da3697
OriCruz <oc080901@gmail.com>
2023-01-06 15:18:10 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import './App.css';
+import { useState } from 'react';
+import GroceryItem from './componets/GroceryItem';
+import groceries from './data'
+
+function App() {
+  
+// setting up and using state, state's initial value is the array of groceries
+  const [grList, setGrList] = useState(groceries)
+ 
+  const addItem = e => {
+    e.preventDefault()
+    setGrList(grList.concat([
+      {
+      item: document.querySelector('input[name="item"]').value,
+      brand: document.querySelector('input[name="brand"]').value,
+      units: document.querySelector('input[name="units"]').value,
+      quantity: document.querySelector('input[name="quantity"]').value,
+      isPurchased:false
+      
+    }
+  ])
+  )
+  console.log(grList)}
+
+  return (
+    
+    <div className="App">
+      <h1> My Grocery List</h1>
+    {grList.map((ele, idx) => {
+  
+  return( !ele.isPurchased && <GroceryItem ele={ele} key={idx}/>  )
+
+})}
+
+    <form onSubmit={addItem}>
+            <h3>Add an item to your list</h3>
+            <p>Item:</p> <input type='text' name="item"></input> <br/>
+            <p>Brand:</p> <input type='text' name="brand"></input><br/>
+            <p>Units:</p> <input type='text' name="units"></input>
+            <br/>
+            <p>Quantity:</p> <input type='number' name="quantity"></input><br/>
+            <input type='submit' value='submit'></input>
+        </form>
+
+      </div>
+  );
+}
+
+export default App
---
+++ src/data.js
@@ -0,0 +1,21 @@
+export default [{
+    item:'Bread',
+    brand:'Sara Lee',
+    untis:'Loaf',
+    quantity:0,
+    isPurchased:false
+},
+{   
+    item:'Orange Jucice',
+    brand:'Tropicana',
+    untis:'Gallon',
+    quantity:0,
+    isPurchased:false
+},
+{   item:'Eggs',
+    brand:'Vital Farms',
+    untis:'Grams',
+    quantity:0,
+    isPurchased:false   
+}
+]
